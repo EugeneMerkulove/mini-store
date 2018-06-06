e42ed789036682d7d2d6046651c2e4c900ace40f
Eugene Merkulove <merkuloveko@gmail.com>
2018-06-06 21:21:56 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.slider-top').slick({
+        autoplay: true,
+        autoplaySpeed: 4500,
+        dots: true,
+        arrows: false
+    });
+  });
+  
+ $(document).ready(function(){
+  $('.slider-btn').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4500,
+    arrows: true,
+  });
+ });
+ 
